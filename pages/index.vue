c371c121313a04f276c4d61c7a584b207dcc8eec
tomavey <tomavey@outlook.com>
2025-06-16 07:50:27 -0400

    test

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@
     />
     <h1 class="text-4xl font-bold mb-2 text-gray-900">Welcome to Everhost</h1>
     <p class="text-lg text-gray-600 mb-8">
-      Your site is deployed and ready to go!
+      Your site is deployed and ready to go!!
     </p>
     <div class="bg-white rounded-lg shadow p-6 max-w-md w-full text-center">
       <p class="text-gray-700 mb-2">
