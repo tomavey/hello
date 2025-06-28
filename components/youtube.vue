9a093dbb24369b7f93c41c9d60e9428fe72a7bec
tomavey <tomavey@outlook.com>
2025-06-28 16:48:21 -0400

    make youtube modal component

---
+++ components/youtube.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+    id="heroVid"
+    class="et_pb_section et_pb_section_1 popup et_pb_with_background et_section_regular"
+  >
+    <div class="et_pb_row et_pb_row_1">
+      <div
+        class="et_pb_column et_pb_column_4_4 et_pb_column_2 et_pb_css_mix_blend_mode_passthrough et-last-child"
+      >
+        <div
+          class="et_pb_module et_pb_text et_pb_text_2 et_animated et_pb_text_align_center et_pb_bg_layout_light"
+        >
+          <div class="et_pb_text_inner">
+            <p>
+              With Everhost you can create a welcome book in less than 10
+              MINUTES!
+            </p>
+          </div>
+        </div>
+        <div class="et_pb_module et_pb_video et_pb_video_0">
+          <div class="et_pb_video_box">
+            <iframe
+              width="100%"
+              height="400"
+              src="https://www.youtube.com/embed/1Iwa79l3mZI"
+              title="Everhost Demo"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
